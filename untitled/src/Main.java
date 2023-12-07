277c147362737b05ad0eb1b5fb7c7e3b28c88a0d
fatemezahrafathian <126403410+fatemezahrafathian@users.noreply.github.com>
2023-12-07 13:56:04 +0330

    add infixToPostfix function

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -72,7 +72,7 @@ public class Main {
                     }
                 }
             }
-            else if (fraze.charAt(i)=='+' || fraze.charAt(i)!='-' || fraze.charAt(i)!='*' ||fraze.charAt(i)!='/' || fraze.charAt(i)!='^'){
+            else if (fraze.charAt(i)=='+' || fraze.charAt(i)=='-' || fraze.charAt(i)=='*' ||fraze.charAt(i)=='/' || fraze.charAt(i)=='^'){
                 if (i+1<fraze.length()){
                     if (fraze.charAt(i+1)=='+' || fraze.charAt(i+1)=='*' || fraze.charAt(i+1)=='/' || fraze.charAt(i+1)=='!'
                             || fraze.charAt(i+1)=='^' || fraze.charAt(i+1)=='.' || fraze.charAt(i+1)==')'){
@@ -80,7 +80,7 @@ public class Main {
                     }
                 }else validOperator=false;
                 if (i-1>=0){
-                    if (fraze.charAt(i-1)=='+' || fraze.charAt(i-1)=='*' || fraze.charAt(i-1)=='/' || fraze.charAt(i-1)=='!'
+                    if (fraze.charAt(i-1)=='+' || fraze.charAt(i-1)=='*' || fraze.charAt(i-1)=='/'
                             || fraze.charAt(i-1)=='^' || fraze.charAt(i-1)=='.' || fraze.charAt(i-1)=='('){
                         validOperator=false;
                     }
@@ -88,20 +88,20 @@ public class Main {
             }
             else if (fraze.charAt(i)=='!'){
                 if (i+1<fraze.length()){
-                    if (fraze.charAt(i+1)!='+' || fraze.charAt(i+1)!='-' || fraze.charAt(i+1)!='*' || fraze.charAt(i+1)!='/'
-                            || fraze.charAt(i+1)!='!' || fraze.charAt(i+1)!='^' || fraze.charAt(i+1)!=')'){
+                    if (fraze.charAt(i+1)!='+' && fraze.charAt(i+1)!='-' && fraze.charAt(i+1)!='*' && fraze.charAt(i+1)!='/'
+                            && fraze.charAt(i+1)!='!' && fraze.charAt(i+1)!='^' && fraze.charAt(i+1)!=')'){
                         validOperator=false;
                     }
                 }
                 if (i-1>=0){
-                    if (fraze.charAt(i-1)!='0' || fraze.charAt(i-1)!='1' || fraze.charAt(i-1)!='2' || fraze.charAt(i-1)!='3'
-                            || fraze.charAt(i-1)!='4' || fraze.charAt(i-1)!='5' || fraze.charAt(i-1)!='6'
-                            || fraze.charAt(i-1)!='7' || fraze.charAt(i-1)!='8' || fraze.charAt(i-1)!='9'){
+                    if (fraze.charAt(i-1)!='0' && fraze.charAt(i-1)!='1' && fraze.charAt(i-1)!='2' && fraze.charAt(i-1)!='3'
+                            && fraze.charAt(i-1)!='4' && fraze.charAt(i-1)!='5' && fraze.charAt(i-1)!='6'
+                            && fraze.charAt(i-1)!='7' && fraze.charAt(i-1)!='8' && fraze.charAt(i-1)!='9'){
                         validOperator=false;
                     }
                 }else validOperator=false;
             }
-            else if (fraze.charAt(i)!='.'){
+            else if (fraze.charAt(i)=='.'){
                 if (i+1<fraze.length()){
                     if (fraze.charAt(i+1)!='0' || fraze.charAt(i+1)!='1' || fraze.charAt(i+1)!='2' || fraze.charAt(i+1)!='3'
                             || fraze.charAt(i+1)!='4' || fraze.charAt(i+1)!='5' || fraze.charAt(i+1)!='6'
@@ -119,5 +119,66 @@ public class Main {
             }
         }
         return validOperator;
+    }
+    private static String infixToPostfix(String infix){
+        LinkedStack<Character> stack = new LinkedStack<>();
+        StringBuilder postfix = new StringBuilder();
+        char character;
+        for (int i = 0 ; i< infix.length() ; i++){
+            character = infix.charAt(i);
+            if(character!=')' && character!='(' && character!='+' && character!='-' && character!='*'
+                    && character!='/' && character!='^' && infix.charAt(i)!='!'){
+                postfix.append(character);
+            }
+            else if ( character=='('){
+                stack.push(character);
+            }
+            else if ( character==')'){
+                while (!stack.isEmpty()){
+                    char t = stack.pop();
+                    if (t != '(')
+                        postfix.append(t);
+                    else break;
+                }
+            }else if (character=='+' || character=='-' || character=='*' || character=='/'
+                    || character=='^' || character=='!'){
+                if (stack.isEmpty()){
+                    stack.push(character);
+                }else {
+                    while (!stack.isEmpty()){
+                        char t = stack.pop();
+                        if (t == '('){
+                            stack.push(t);
+                            break;
+                        }else if (t=='+' || t=='-' || t=='*' || t=='/'
+                                || t=='^' || t=='!'){
+                            if (getPriority(t)<getPriority(character)){
+                                stack.push(t);
+                                break;
+                            }else
+
+                                postfix.append(t);
+                        }
+                    }
+                    stack.push(character);
+                }
+            }
+        }
+        while (!stack.isEmpty()){
+            postfix.append(stack.pop());
+        }
+        return postfix.toString();
+    }
+    private static int getPriority(char c){
+        if (c=='!')
+            return 4;
+        else if (c=='^')
+            return 3;
+        else if (c=='*' || c=='/')
+            return 2;
+        else if (c=='+' || c=='-')
+            return 1;
+        else
+            return 5;
     }
 }
