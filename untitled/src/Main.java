0739d599b83e86d674a6e2f5a85813e5cc1d282a
fatemezahrafathian <126403410+fatemezahrafathian@users.noreply.github.com>
2023-12-07 14:25:42 +0330

    add isNumber ,replaceE,replacePI,splitFraze function

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,6 +1,70 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String string = scanner.nextLine();
+        System.out.println(validParenthesize(string));
+        System.out.println(validElements(string));
+        System.out.println(validOperator(string));
+
+        if (validParenthesize(string) && validElements(string) && validOperator(string) ){
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i=0 ; i< string.length() ; i++){
+                stringBuilder.append(string.charAt(i));
+            }
+            string=replaceE(stringBuilder);
+            string=replacePI(stringBuilder);
+            string=splitFraze(string);
+            String postfix=infixToPostfix(string);
+            System.out.println(postfix);;
+            String[] operandAndOperators = postfix.split(",");
+            LinkedStack<String> stack = new LinkedStack<>();
+            LinkedStack<Double> tempStack = new LinkedStack<>();
+            for (int i=operandAndOperators.length -1 ; i>=0 ; i--){
+                stack.push(operandAndOperators[i]);
+            }
+            try {
+                while (!stack.isEmpty()){
+                    while (isNumber(stack.top().charAt(0)) ||(stack.top().charAt(0)=='-'&& stack.top().length()>1)){
+                        tempStack.push(Double.parseDouble(stack.pop()));
+                    }
+                    if (stack.top().charAt(0)=='+'){
+                        tempStack.push(tempStack.pop()+tempStack.pop());
+                        stack.pop();
+                    }
+                    else if (stack.top().charAt(0)=='-' && stack.top().length()==1){
+                        double a=tempStack.pop();
+                        double b=tempStack.pop();
+                        tempStack.push(b-a);
+                        stack.pop();
+                    }else if (stack.top().charAt(0)=='*'){
+                        tempStack.push(tempStack.pop()*tempStack.pop());
+                        stack.pop();
+                    }else if (stack.top().charAt(0)=='/'){
+                        double a=tempStack.pop();
+                        double b=tempStack.pop();
+                        tempStack.push(b/a);
+                        stack.pop();
+                    }else if (stack.top().charAt(0)=='^'){
+                        double a=tempStack.pop();
+                        double b=tempStack.pop();
+                        tempStack.push(Math.pow(b,a));
+                        stack.pop();
+                    }else if (stack.top().charAt(0)=='!'){
+                        int a=tempStack.pop().intValue();
+                        tempStack.push(factorial(a));
+                        stack.pop();
+                    }
+                }
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+            System.out.println(tempStack.pop());
+        }
     }
     private static boolean validParenthesize(String fraze){
         //save ) elements
@@ -32,8 +96,16 @@ public class Main {
                     && fraze.charAt(i)!='-' && fraze.charAt(i)!='*' && fraze.charAt(i)!='/' && fraze.charAt(i)!='^'
                     && fraze.charAt(i)!='!' && fraze.charAt(i)!='.' && fraze.charAt(i)!='0' && fraze.charAt(i)!='1'
                     && fraze.charAt(i)!='2' && fraze.charAt(i)!='3' && fraze.charAt(i)!='4' && fraze.charAt(i)!='5' && fraze.charAt(i)!='6'
-                    && fraze.charAt(i)!='7' && fraze.charAt(i)!='8' && fraze.charAt(i)!='9'){
+                    && fraze.charAt(i)!='7' && fraze.charAt(i)!='8' && fraze.charAt(i)!='9' && fraze.charAt(i)!='e'&& fraze.charAt(i)!='P'){
                 invalidElement=false;
+            }
+            if (fraze.charAt(i)=='P'){
+                if (i+1>=fraze.length())
+                    invalidElement=false;
+                else if (fraze.charAt(i+1)!='I'){
+                    invalidElement=false;
+                }
+                i++;
             }
         }
         return invalidElement;
@@ -59,7 +131,7 @@ public class Main {
             }
             else if (fraze.charAt(i)=='('){
                 if (i+1<fraze.length()){
-                    if (fraze.charAt(i+1)=='+' || fraze.charAt(i+1)=='-' || fraze.charAt(i+1)=='*' || fraze.charAt(i+1)=='/'
+                    if (fraze.charAt(i+1)=='+'  || fraze.charAt(i+1)=='*' || fraze.charAt(i+1)=='/'
                             || fraze.charAt(i+1)=='!' || fraze.charAt(i+1)=='^' || fraze.charAt(i+1)=='.' || fraze.charAt(i+1)==')'){
                         validOperator=false;
                     }
@@ -72,7 +144,20 @@ public class Main {
                     }
                 }
             }
-            else if (fraze.charAt(i)=='+' || fraze.charAt(i)=='-' || fraze.charAt(i)=='*' ||fraze.charAt(i)=='/' || fraze.charAt(i)=='^'){
+            else if (fraze.charAt(i)=='-'){
+                if (i+1<fraze.length()){
+                    if (fraze.charAt(i+1)=='+' || fraze.charAt(i+1)=='*' || fraze.charAt(i+1)=='/' || fraze.charAt(i+1)=='!'
+                            || fraze.charAt(i+1)=='^' || fraze.charAt(i+1)=='.' || fraze.charAt(i+1)==')'){
+                        validOperator=false;
+                    }
+                }else validOperator=false;
+                if (i-1>=0){
+                    if (fraze.charAt(i-1)=='.'){
+                        validOperator=false;
+                    }
+                }
+            }
+            else if (fraze.charAt(i)=='+'|| fraze.charAt(i)=='*' ||fraze.charAt(i)=='/' || fraze.charAt(i)=='^'){
                 if (i+1<fraze.length()){
                     if (fraze.charAt(i+1)=='+' || fraze.charAt(i+1)=='*' || fraze.charAt(i+1)=='/' || fraze.charAt(i+1)=='!'
                             || fraze.charAt(i+1)=='^' || fraze.charAt(i+1)=='.' || fraze.charAt(i+1)==')'){
@@ -103,16 +188,16 @@ public class Main {
             }
             else if (fraze.charAt(i)=='.'){
                 if (i+1<fraze.length()){
-                    if (fraze.charAt(i+1)!='0' || fraze.charAt(i+1)!='1' || fraze.charAt(i+1)!='2' || fraze.charAt(i+1)!='3'
-                            || fraze.charAt(i+1)!='4' || fraze.charAt(i+1)!='5' || fraze.charAt(i+1)!='6'
-                            || fraze.charAt(i+1)!='7' || fraze.charAt(i+1)!='8' || fraze.charAt(i+1)!='9'){
+                    if (fraze.charAt(i+1)!='0' && fraze.charAt(i+1)!='1' && fraze.charAt(i+1)!='2' && fraze.charAt(i+1)!='3'
+                            && fraze.charAt(i+1)!='4' && fraze.charAt(i+1)!='5' && fraze.charAt(i+1)!='6'
+                            && fraze.charAt(i+1)!='7' && fraze.charAt(i+1)!='8' && fraze.charAt(i+1)!='9'){
                         validOperator=false;
                     }
                 }else validOperator=false;
                 if (i-1>=0){
-                    if (fraze.charAt(i-1)!='0' || fraze.charAt(i-1)!='1' || fraze.charAt(i-1)!='2' || fraze.charAt(i-1)!='3'
-                            || fraze.charAt(i-1)!='4' || fraze.charAt(i-1)!='5' || fraze.charAt(i-1)!='6'
-                            || fraze.charAt(i-1)!='7' || fraze.charAt(i-1)!='8' || fraze.charAt(i-1)!='9'){
+                    if (fraze.charAt(i-1)!='0' && fraze.charAt(i-1)!='1' && fraze.charAt(i-1)!='2' && fraze.charAt(i-1)!='3'
+                            && fraze.charAt(i-1)!='4' && fraze.charAt(i-1)!='5' && fraze.charAt(i-1)!='6'
+                            && fraze.charAt(i-1)!='7' && fraze.charAt(i-1)!='8' && fraze.charAt(i-1)!='9'){
                         validOperator=false;
                     }
                 }else validOperator=false;
@@ -120,13 +205,50 @@ public class Main {
         }
         return validOperator;
     }
-    private static String infixToPostfix(String infix){
+    private static String replaceE(StringBuilder fraze){
+        for (int i = 0 ; i<fraze.length(); i++){
+            if (fraze.charAt(i)=='e'){
+                fraze.delete(i,i+1);
+                fraze.insert(i,"2.71");
+            }
+        }
+        return fraze.toString();
+    }
+    private static String replacePI(StringBuilder fraze){
+        for (int i = 0 ; i<fraze.length(); i++){
+            if (fraze.charAt(i)=='P'){
+                fraze.delete(i,i+2);
+                fraze.insert(i,"3.14");
+            }
+        }
+        return fraze.toString();
+    }
+    private static String splitFraze(String fraze){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i=0 ; i<fraze.length() ; i++){
+            stringBuilder.append(fraze.charAt(i));
+            if (isNumber(fraze.charAt(i))){
+                if (i+1<fraze.length()){
+                    if (!isNumber(fraze.charAt(i+1)) && fraze.charAt(i+1)!='.'){
+                        stringBuilder.append(",");
+                    }
+                }else stringBuilder.append(",");
+            }
+
+        }
+        return stringBuilder.toString();
+    }
+    private static String infixToPostfix(String s){
+        StringBuilder infix = new StringBuilder();
+        for (int i = 0 ; i < s.length() ; i++){
+            infix.append(s.charAt(i));
+        }
         LinkedStack<Character> stack = new LinkedStack<>();
         StringBuilder postfix = new StringBuilder();
         char character;
         for (int i = 0 ; i< infix.length() ; i++){
             character = infix.charAt(i);
-            if(character!=')' && character!='(' && character!='+' && character!='-' && character!='*'
+            if(character!=')' && character!='(' && character!='+' && character!='-'  && character!='*'
                     && character!='/' && character!='^' && infix.charAt(i)!='!'){
                 postfix.append(character);
             }
@@ -137,11 +259,60 @@ public class Main {
                 while (!stack.isEmpty()){
                     char t = stack.pop();
                     if (t != '(')
-                        postfix.append(t);
+                        postfix.append(t).append(",");
                     else break;
                 }
-            }else if (character=='+' || character=='-' || character=='*' || character=='/'
-                    || character=='^' || character=='!'){
+            }
+            else if (character=='-'){
+                if (infix.charAt(i + 1) == '(') {
+                    infix.insert(i+1,'*');
+                    infix.insert(i+1,',');
+                    infix.insert(i+1,'1');
+                    infix.insert(i+1,'-');
+                    if (i-2>=0){
+                        if (isNumber(infix.charAt(i-2))){
+                            infix.insert(i+1,'+');
+                        }
+                    }
+
+                }
+                else if (i==0){
+                    postfix.append(character);
+                }else {
+                    if (infix.charAt(i-1)=='+'||infix.charAt(i-1)=='-'||infix.charAt(i-1)=='*'||infix.charAt(i-1)=='/'||infix.charAt(i-1)=='^'||infix.charAt(i-1)=='(')
+                        postfix.append(character);
+                    else if (infix.charAt(i + 1) == '(') {
+
+                        infix.insert(i+1,'*');
+                        infix.insert(i+1,',');
+                        infix.insert(i+1,'1');
+                        infix.insert(i+1,'-');
+                        if (isNumber(infix.charAt(i-1))){
+                            infix.insert(i+1,',');
+                            infix.insert(i+1,'+');
+
+                        }
+                    } else {
+                        while (!stack.isEmpty()){
+                            char t = stack.pop();
+                            if (t == '('){
+                                stack.push(t);
+                                break;
+                            }else if (t=='+' || t=='-' || t=='*' || t=='/'
+                                    || t=='^' || t=='!'){
+                                if (getPriority(t)<getPriority(character)){
+                                    stack.push(t);
+                                    break;
+                                }else
+
+                                    postfix.append(t).append(",");
+                            }
+                        }
+                        stack.push(character);
+                    }
+                }
+            }
+            else if (character=='+'  || character=='*' || character=='/' || character=='^' || character=='!'){
                 if (stack.isEmpty()){
                     stack.push(character);
                 }else {
@@ -157,7 +328,7 @@ public class Main {
                                 break;
                             }else
 
-                                postfix.append(t);
+                                postfix.append(t).append(",");
                         }
                     }
                     stack.push(character);
@@ -165,9 +336,16 @@ public class Main {
             }
         }
         while (!stack.isEmpty()){
-            postfix.append(stack.pop());
+            postfix.append(stack.pop()).append(",");
         }
         return postfix.toString();
+    }
+    private static boolean isNumber(char character) {
+        if (character=='0' || character=='1' || character=='2' || character=='3' || character=='4' || character=='5'
+                || character=='6' || character=='7' || character=='8' || character=='9' ){
+            return true;
+        }
+        return false;
     }
     private static int getPriority(char c){
         if (c=='!')
@@ -181,4 +359,12 @@ public class Main {
         else
             return 5;
     }
+    public static double factorial(int num)
+    {
+        if (num >= 1)
+            return num * factorial(num - 1);
+        else
+            return 1;
+    }
+
 }
